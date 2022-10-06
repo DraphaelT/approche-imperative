24f6e48cb0dc6abe91ca6238229c3947d98f7042
DraphaelT <115068828+DraphaelT@users.noreply.github.com>
2022-10-06 16:38:15 +0200

    TP3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,31 @@
+package fr.declaration.variable;
+
+import java.awt.desktop.SystemEventListener;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 
+		 * Exercice 1
+		 */
+		byte variable1 = 1;
+		short variable2 = 10;
+		int variable3 = 100;
+		long variable4 = 1000;
+		float variable5 = 10.5F;
+		double variable6 = 100.500F;
+		char variable7 = 'â';
+		boolean variable8 = true;
+		String variable9 = "Je suis un phrase ou je suis 0";
+		
+		System.out.println(variable2 + " " +variable7);
+		/*
+		 * Exercice 2
+		 */
+		String randomString= "Voici le résultat d’un calcul : \n1+5=6";
+		System.out.println(randomString);
+		
+	}
+
+}
