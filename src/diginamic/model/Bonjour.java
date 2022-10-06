16f8b689ba7b791b61399c069c9e1e932d2337cb
DraphaelT <115068828+DraphaelT@users.noreply.github.com>
2022-10-06 15:36:02 +0200

    Init

---
+++ src/diginamic/model/Bonjour.java
@@ -0,0 +1,15 @@
+package diginamic.model;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New World");
+		
+		int Batiment = 053 ; // Notation octale pour les nombres entiers
+		System.out.println(Batiment);
+		double b = 1;
+		
+	}
+
+}
